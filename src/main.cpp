efd18f2853ac62ca277c8a8fafc643a6f24fa463
aaron mathankeri <aaron@quantifiedag.com>
2017-08-08 12:48:01 -0500

    Design Matrix calculation is now a function.
    This will eventually allow a user to specify what type of basis function
    they want to use. Much easier to implement different types of basis functions
    with this functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,10 @@ const int NUM_PATTERNS = 10;
 const int ORDER = 2;
 
 //TODO:
-// 1. computing design matrix can be abstracted
+// 1. computing design matrix can be abstracted [complete]
 // 2. computing inverse can be abstracted
 // 3. solving Normal equations can be abstracted
-
+// 4. Steps 2 & 3 are 'Moore-Penrose psuedo-inverse'. make it one function call!
 void printVector( const double *x , const int length ){
       for (int i = 0; i < length; i++) {
 	    printf ("%12.3f", x[i]);
@@ -49,6 +49,26 @@ void loadData( double *x , string fileName ){
       }
 }
 
+double *computeDesignMatrix( double *x ){
+     double *Phi = (double *)mkl_malloc( NUM_PATTERNS*ORDER*sizeof( double ), 64 );
+     memset( Phi, 0.0, NUM_PATTERNS * ORDER* sizeof(double));     
+      //set first column to 1--dummy index to calculate w0
+      // maybe in an opportunity for blas routines?
+      for (int i = 0; i < NUM_PATTERNS*ORDER; ++i) {
+	    Phi[i] = 1.0;
+	    i++;
+      }
+      // phi(x) = x--> identity basis function for linear regression
+      for (int i = 0; i < NUM_PATTERNS; ++i) {
+	    for (int j = 0; j < ORDER; ++j) {
+		  if (( j % 2) != 0) {
+			Phi[i*ORDER + j] = x[i];			
+		  }
+	    }
+      }
+      return Phi;
+}
+
 int main(int argc, char *argv[])
 {
       cout << " Aaron's Back." << endl;
@@ -71,7 +91,7 @@ int main(int argc, char *argv[])
       x = (double *)mkl_malloc( NUM_PATTERNS*sizeof( double ), 64 );
       t = (double *)mkl_malloc( NUM_PATTERNS*sizeof( double ), 64 );
       w = (double *)mkl_malloc( (ORDER)*sizeof( double ), 64 );
-      designMatrix = (double *)mkl_malloc( NUM_PATTERNS*ORDER*sizeof( double ), 64 );
+
       designTranspose = (double *)mkl_malloc( ORDER*NUM_PATTERNS*sizeof( double ), 64 );
       A = (double *)mkl_malloc( ORDER*ORDER*sizeof( double ), 64 );
       B = (double *)mkl_malloc( ORDER*sizeof( double ), 64 );
@@ -81,7 +101,7 @@ int main(int argc, char *argv[])
       memset( x, 0.0,  NUM_PATTERNS * sizeof(double));
       memset( t, 0.0,  NUM_PATTERNS * sizeof(double));
       memset( w, 0.0,  (ORDER) * sizeof(double));      
-      memset( designMatrix, 0.0, NUM_PATTERNS * ORDER* sizeof(double));
+     
       memset( designTranspose, 0.0, ORDER* NUM_PATTERNS*sizeof(double));
       memset( A, 0.0, ORDER* ORDER*sizeof(double));
       memset( B, 0.0, ORDER*sizeof(double));
@@ -97,25 +117,11 @@ int main(int argc, char *argv[])
 
       //--------------------------------------------------------------------------------
       cout << "\nComputing Design Matrix ..." << endl;
-      //set first column to 1--dummy index to calculate w0
-      // maybe in an opportunity for blas routines?
-      for (int i = 0; i < NUM_PATTERNS*ORDER; ++i) {
-	    designMatrix[i] = 1.0;
-	    i++;
-      }
-
-      // phi(x) = x--> identity basis function for linear regression
-      for (int i = 0; i < NUM_PATTERNS; ++i) {
-	    for (int j = 0; j < ORDER; ++j) {
-		  if (( j % 2) != 0) {
-			designMatrix[i*ORDER + j] = x[i];			
-		  }
-	    }
-      }
-
+      designMatrix = computeDesignMatrix( x );
       printf ("\n Design Matrix: \n");
       printMatrix( designMatrix, NUM_PATTERNS, ORDER );
       //--------------------------------------------------------------------------------
+      //*
       // compute matrices for w_ml calculations
       // 1. need Phi_Transpose
       mkl_domatcopy('R' , 'T' ,  NUM_PATTERNS, ORDER, alpha,
@@ -142,8 +148,9 @@ int main(int argc, char *argv[])
 
       printf ("\n Inverse A : \n");
       printMatrix( A, ORDER, ORDER );
-
+      //--------------------------------------------------------------------------------
       //get B
+
       cblas_dgemv( CblasRowMajor, CblasNoTrans, ORDER, NUM_PATTERNS,
 		   alpha, designTranspose, NUM_PATTERNS, t, 1, beta, B, 1);
 
@@ -154,9 +161,11 @@ int main(int argc, char *argv[])
       cblas_dgemv( CblasRowMajor, CblasNoTrans, ORDER, ORDER,
 		   alpha, A, ORDER, B, 1, beta, w, 1);
       //--------------------------------------------------------------------------------
+
       cout << "\nComputing Line of Best Fit ..." << endl;
       cout << " w0 = " << w[0] << endl;
       cout << " w1 = " << w[1] << endl;
+
       //--------------------------------------------------------------------------------
       printf ("\n Deallocating memory \n\n");
       mkl_free( x );
